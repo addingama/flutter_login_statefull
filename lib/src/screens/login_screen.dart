8e6d6c1046ed0037ef298f614f07637ec6376d46
Addin Gama Bertaqwa <punyagama@gmail.com>
2019-03-23 09:22:13 +0800

    Add form and input field

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(BuildContext context) {
+
+    return Container(
+      margin:EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            // passwordField(),
+            // submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com'
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    
+  }
+
+  Widget submitButton() {
+
+  }
+}
